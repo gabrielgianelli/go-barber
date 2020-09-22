5206ce24aecd33261ac345785b3297ff21dae537
Gabriel Gianelli <gabrielgianelli@gmail.com>
2020-09-22 18:39:56 -0300

    refactor: change AuthContext imports

--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -6,7 +6,7 @@ import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 import { Container, Content, Background } from './styles';
 
