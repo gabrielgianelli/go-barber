6216fee8257df6e48474faeed1defe95d7947fac
Gabriel Gianelli <gabrielgianelli@gmail.com>
2020-09-23 20:01:16 -0300

    feat: create authenticated route

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Route from './Route';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
