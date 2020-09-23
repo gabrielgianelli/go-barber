320bc961f3b2c7b93f704fd927b5a38691e7f4c3
Gabriel Gianelli <gabrielgianelli@gmail.com>
2020-09-23 11:05:20 -0300

    refactor: rename hooks files

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
-import { AuthProvider } from './hooks/AuthContext';
+import { AuthProvider } from './hooks/auth';
 import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -6,7 +6,7 @@ import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
 import getValidationErrors from '../../utils/getValidationErrors';
 import { Container, Content, Background } from './styles';
 
