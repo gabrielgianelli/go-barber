ce141efbeb76ffb3686a9dcc5844b96cb00faee2
Gabriel Gianelli <gabrielgianelli@gmail.com>
2020-09-22 23:54:40 -0300

    feat: create Toast component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
 
     <GlobalStyle />
   </>
--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -38,8 +38,11 @@ const SignIn: React.FC = () => {
 
         signIn({ email: data.email, password: data.password });
       } catch (err) {
-        const errors = getValidationErrors(err);
-        formRef.current?.setErrors(errors);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+        }
       }
     },
     [signIn],
