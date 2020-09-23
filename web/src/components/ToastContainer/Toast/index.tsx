93c7fbfbde347a677dd7e4d920e9cb6c8dc9c327
Gabriel Gianelli <gabrielgianelli@gmail.com>
2020-09-23 14:24:53 -0300

    refactor: move toast component to subdirectory

---
+++ web/src/components/ToastContainer/Toast/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+import { ToastMessage, useToast } from '../../../hooks/toast';
+import Container from './styles';
+
+interface ToastProps {
+  message: ToastMessage;
+}
+
+const Toast: React.FC<ToastProps> = ({ message }) => {
+  const { removeToast } = useToast();
+
+  return (
+    <Container type={message.type} hasDescription={!!message.description}>
+      <FiAlertCircle size={20} />
+      <div>
+        <strong>{message.title}</strong>
+        {message.description && <p>{message.description}</p>}
+      </div>
+
+      <button type="button" onClick={() => removeToast(message.id)}>
+        <FiXCircle size={18} />
+      </button>
+    </Container>
+  );
+};
+
+export default Toast;
