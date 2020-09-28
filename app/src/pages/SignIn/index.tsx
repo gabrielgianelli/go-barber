a0227d1906ec6e69e644bf01130c3cf0d21c49b2
Gabriel Gianelli <gabrielgianelli@gmail.com>
2020-09-28 19:46:50 -0300

    feat: add forgot password and create account links

--- app/src/pages/SignIn/index.tsx
+++ app/src/pages/SignIn/index.tsx
@@ -1,28 +1,65 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  View,
+  ScrollView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logoImg from '../../assets/logo.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  CreateAccountButton,
+  CreateAccountButtonText,
+  ForgotPassword,
+  ForgotPasswordText,
+  Title,
+} from './styles';
 
 const SignIn: React.FC = () => (
-  <Container>
-    <Image source={logoImg} />
-    <Title>Faça seu logon</Title>
-
-    <Input name="email" icon="mail" placeholder="E-mail" />
-    <Input name="password" icon="lock" placeholder="Senha" />
-
-    <Button
-      onPress={() => {
-        console.log('Deu');
-      }}
+  <>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
     >
-      Entrar
-    </Button>
-  </Container>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Image source={logoImg} />
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
+
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button
+            onPress={() => {
+              console.log('Deu');
+            }}
+          >
+            Entrar
+          </Button>
+
+          <ForgotPassword>
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+    <CreateAccountButton>
+      <Icon name="log-in" size={20} color="#ff9000" />
+      <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+    </CreateAccountButton>
+  </>
 );
 
 export default SignIn;
