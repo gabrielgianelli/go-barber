9f77e3209423959632bbf83172a04b34d8068883
Gabriel Gianelli <gabrielgianelli@gmail.com>
2020-09-28 22:11:24 -0300

    feat: add Sign Up page

--- app/src/pages/SignUp/index.tsx
+++ app/src/pages/SignUp/index.tsx
@@ -1,8 +1,55 @@
 import React from 'react';
-import { View } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  View,
+  ScrollView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-const SignUp: React.FC = () => <View />;
+import { Container, BackToSignIn, BackToSignInText, Title } from './styles';
+
+const SignUp: React.FC = () => (
+  <>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Image source={logoImg} />
+          <View>
+            <Title>Crie sua conta</Title>
+          </View>
+
+          <Input name="name" icon="user" placeholder="Nome" />
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button
+            onPress={() => {
+              console.log('Deu');
+            }}
+          >
+            Entrar
+          </Button>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+    <BackToSignIn>
+      <Icon name="arrow-left" size={20} color="#fff" />
+      <BackToSignInText>Voltar para logon</BackToSignInText>
+    </BackToSignIn>
+  </>
+);
 
 export default SignUp;
