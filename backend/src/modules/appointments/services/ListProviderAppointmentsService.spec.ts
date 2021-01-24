87ed2a29a1f6f5b0037f82ec1cbc856e5d709ca8
Gabriel Gianelli <gabrielgianelli@gmail.com>
2021-01-24 00:44:01 -0300

    feat: list provider appointments

---
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,55 @@
+import { getYear } from 'date-fns';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+let year: number;
+let month: number;
+let day: number;
+
+let provider_id: string;
+let user_id: string;
+let date1: Date;
+let date2: Date;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+
+    year = getYear(new Date()) + 1;
+    month = 4;
+    day = 25;
+
+    provider_id = 'provider';
+    user_id = 'user';
+    date1 = new Date(year, month, day, 11);
+    date2 = new Date(year, month, day, 14);
+  });
+
+  it('should be able to list the appointments on a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id,
+      user_id,
+      date: date1,
+    });
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id,
+      user_id,
+      date: date2,
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id,
+      year,
+      month: month + 1,
+      day,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
