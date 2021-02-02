8bf84ed8d8c5dc93a070520d44bf449fc330c977
Gabriel Gianelli <gabrielgianelli@gmail.com>
2021-02-02 01:29:06 -0300

    feat: add reset password page

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Route from './Route';
@@ -10,6 +11,7 @@ const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
     <Route path="/signup" component={SignUp} />
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
