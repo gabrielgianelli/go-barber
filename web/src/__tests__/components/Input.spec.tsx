eb20f116a4cd738b8533ed63d98d686dea60954d
Gabriel Gianelli <gabrielgianelli@gmail.com>
2021-02-16 02:15:37 -0300

    test: add component input test

---
+++ web/src/__tests__/components/Input.spec.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { fireEvent, render, wait, waitFor } from '@testing-library/react';
+import Input from '../../components/Input';
+
+jest.mock('@unform/core', () => {
+  return {
+    useField() {
+      return {
+        fieldName: 'email',
+        defaultValue: '',
+        error: '',
+        registerField: jest.fn(),
+      };
+    },
+  };
+});
+
+describe('Input Component', () => {
+  it('should be able to render an input', () => {
+    const { getByPlaceholderText } = render(
+      <Input name="email" placeholder="Email" />,
+    );
+
+    expect(getByPlaceholderText('Email')).toBeTruthy();
+  });
+
+  it('should render highlight on input focus', async () => {
+    const { getByPlaceholderText, getByTestId } = render(
+      <Input name="email" placeholder="Email" />,
+    );
+
+    const inputElement = getByPlaceholderText('Email');
+    const containerElement = getByTestId('input-container');
+
+    fireEvent.focus(inputElement);
+
+    await waitFor(() => {
+      expect(containerElement).toHaveStyle('border-color: #ff9000;');
+      expect(containerElement).toHaveStyle('color: #ff9000;');
+    });
+
+    fireEvent.blur(inputElement);
+
+    await waitFor(() => {
+      expect(containerElement).not.toHaveStyle('border-color: #ff9000;');
+      expect(containerElement).not.toHaveStyle('color: #ff9000;');
+    });
+  });
+
+  it('should keep input border highlight when input filled', async () => {
+    const { getByPlaceholderText, getByTestId } = render(
+      <Input name="email" placeholder="Email" />,
+    );
+
+    const inputElement = getByPlaceholderText('Email');
+    const containerElement = getByTestId('input-container');
+
+    fireEvent.change(inputElement, {
+      target: {
+        value: 'john.doe@email.com',
+      },
+    });
+
+    fireEvent.blur(inputElement);
+
+    await waitFor(() => {
+      expect(containerElement).toHaveStyle('color: #ff9000;');
+    });
+  });
+});
