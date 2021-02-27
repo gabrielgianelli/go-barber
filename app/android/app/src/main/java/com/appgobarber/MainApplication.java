069fc10baec796ed7aab26c3e9301cf04ac594a3
Gabriel Gianelli <gabrielgianelli@gmail.com>
2021-02-27 16:47:49 -0300

    chore: configure code push

--- app/android/app/src/main/java/com/appgobarber/MainApplication.java
+++ app/android/app/src/main/java/com/appgobarber/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.microsoft.codepush.react.CodePush;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -18,6 +19,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
         }
 
         @Override
